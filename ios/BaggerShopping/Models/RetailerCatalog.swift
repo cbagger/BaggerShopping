885c787e2006bbfe9e67415e9bb09d446178a595
cbagger <51855681+cbagger@users.noreply.github.com>
2026-08-20 07:55:36 +0200

    Add shared iOS retailer catalog

---
+++ ios/BaggerShopping/Models/RetailerCatalog.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+enum RetailerCatalog {
+    static let all: [String] = [
+        "MENY",
+        "365discount",
+        "REMA 1000",
+        "Bilka",
+        "føtex",
+        "Lidl",
+        "Netto",
+        "SPAR",
+        "SuperBrugsen",
+        "Kvickly",
+        "Brugsen",
+        "Min Købmand",
+        "LET-KØB"
+    ]
+}
